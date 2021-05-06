ee10627cc03ea78535dc4163ae5971ea05b5c881
adisugiarto <adisugiarto270@gmail.com>
2021-05-06 17:16:32 +0700

    db connection heroku

--- src/main/java/com/example/demo/controller/mvc/BaseMvcController.java
+++ src/main/java/com/example/demo/controller/mvc/BaseMvcController.java
@@ -5,15 +5,15 @@ import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.RequestMapping;
 
 @Controller
-@RequestMapping(value = "/")
+@RequestMapping
 public class BaseMvcController {
 
-    @GetMapping("user")
+    @GetMapping("/user")
     public String user() {
         return "tabel/index";
     }
 
-    @GetMapping("transaksi")
+    @GetMapping("/transaksi")
     public String transaksi() {
         return "tabel/index2";
     }
