34e6b52f0181c8ea62b81b45dcc5c62615f77652
adisugiarto <adisugiarto270@gmail.com>
2021-05-06 17:21:58 +0700

    db connection heroku

--- src/main/java/com/example/demo/MainConfig.java
+++ src/main/java/com/example/demo/MainConfig.java
@@ -11,7 +11,7 @@ import java.net.URISyntaxException;
 public class MainConfig {
 
     @Bean
-    public BasicDataSource dataSource() throws URISyntaxException {
+    public BasicDataSource dataSourceBasic() throws URISyntaxException {
         URI dbUri = new URI(System.getenv("postgres://wbrfbyafhhynaj:db13473e975674aec2d61c9f857165ce28d898d1da8f8f10b901c10ff513b721@ec2-52-21-153-207.compute-1.amazonaws.com:5432/d978t3ehf71lu4"));
 
         String username = dbUri.getUserInfo().split(":")[0];
